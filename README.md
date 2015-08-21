7e1961000834cd149dc583368826a6d506d10ab2
rolaveric <rolaveric@gmail.com>
2015-08-22 02:34:09 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# FalcorJS + Angular v1 example
+
+A quick example of using [FalcorJS](https://netflix.github.io/falcor/) with [AngularJS v1](https://angularjs.org/).
+
+# Setup
+
+Requires JSPM to be installed globally
+
+`npm install -g jspm`
+
+Install dependencies through jspm
+
+`jspm install`
+
+Requires a HTTP server of some kind.  [`http-server`](https://www.npmjs.com/package/http-server) is a good option.
+
+`npm install -g http-server`
+
+Then just run `http-server` and open [http://localhost:8080/index.html](http://localhost:8080/index.html)
---
+++ config.js
@@ -0,0 +1,100 @@
+System.config({
+  "baseURL": "/",
+  "defaultJSExtensions": true,
+  "transpiler": "babel",
+  "babelOptions": {
+    "optional": [
+      "runtime"
+    ]
+  },
+  "paths": {
+    "github:*": "jspm_packages/github/*",
+    "npm:*": "jspm_packages/npm/*"
+  }
+});
+
+System.config({
+  "map": {
+    "angular": "github:angular/bower-angular@1.4.4",
+    "babel": "npm:babel-core@5.8.22",
+    "babel-runtime": "npm:babel-runtime@5.8.20",
+    "core-js": "npm:core-js@0.9.18",
+    "falcor": "npm:falcor@0.1.12",
+    "rx-angular": "npm:rx-angular@0.0.14",
+    "text": "github:systemjs/plugin-text@0.0.2",
+    "github:jspm/nodelibs-buffer@0.1.0": {
+      "buffer": "npm:buffer@3.4.2"
+    },
+    "github:jspm/nodelibs-domain@0.1.0": {
+      "domain-browser": "npm:domain-browser@1.1.4"
+    },
+    "github:jspm/nodelibs-events@0.1.1": {
+      "events": "npm:events@1.0.2"
+    },
+    "github:jspm/nodelibs-path@0.1.0": {
+      "path-browserify": "npm:path-browserify@0.0.0"
+    },
+    "github:jspm/nodelibs-process@0.1.1": {
+      "process": "npm:process@0.10.1"
+    },
+    "github:jspm/nodelibs-util@0.1.0": {
+      "util": "npm:util@0.10.3"
+    },
+    "npm:asap@2.0.3": {
+      "domain": "github:jspm/nodelibs-domain@0.1.0",
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:babel-runtime@5.8.20": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:buffer@3.4.2": {
+      "base64-js": "npm:base64-js@0.0.8",
+      "ieee754": "npm:ieee754@1.1.6",
+      "is-array": "npm:is-array@1.0.1"
+    },
+    "npm:core-js@0.9.18": {
+      "fs": "github:jspm/nodelibs-fs@0.1.2",
+      "process": "github:jspm/nodelibs-process@0.1.1",
+      "systemjs-json": "github:systemjs/plugin-json@0.1.0"
+    },
+    "npm:domain-browser@1.1.4": {
+      "events": "github:jspm/nodelibs-events@0.1.1"
+    },
+    "npm:falcor-json-graph@1.1.5": {
+      "falcor-path-syntax": "npm:falcor-path-syntax@0.2.1"
+    },
+    "npm:falcor@0.1.12": {
+      "asap": "npm:asap@2.0.3",
+      "buffer": "github:jspm/nodelibs-buffer@0.1.0",
+      "falcor-json-graph": "npm:falcor-json-graph@1.1.5",
+      "falcor-path-syntax": "npm:falcor-path-syntax@0.2.1",
+      "falcor-path-utils": "npm:falcor-path-utils@0.3.0",
+      "path": "github:jspm/nodelibs-path@0.1.0",
+      "process": "github:jspm/nodelibs-process@0.1.1",
+      "promise": "npm:promise@7.0.0",
+      "rx": "npm:rx@2.5.3",
+      "util": "github:jspm/nodelibs-util@0.1.0"
+    },
+    "npm:inherits@2.0.1": {
+      "util": "github:jspm/nodelibs-util@0.1.0"
+    },
+    "npm:path-browserify@0.0.0": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:promise@7.0.0": {
+      "asap": "npm:asap@2.0.3",
+      "fs": "github:jspm/nodelibs-fs@0.1.2"
+    },
+    "npm:rx-angular@0.0.14": {
+      "rx": "npm:rx@2.5.3"
+    },
+    "npm:rx@2.5.3": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:util@0.10.3": {
+      "inherits": "npm:inherits@2.0.1",
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    }
+  }
+});
+
